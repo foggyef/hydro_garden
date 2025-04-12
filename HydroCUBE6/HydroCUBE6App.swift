3cff4d054ce405e227d63dc06e16171b4a5d8011
Elliott <Fogiel>
2025-04-11 23:21:05 -0700

    Initial Commit

---
+++ HydroCUBE6/HydroCUBE6App.swift
@@ -0,0 +1,17 @@
+//
+//  HydroCUBE6App.swift
+//  HydroCUBE6
+//
+//  Created by Elliott Fogiel on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HydroCUBE6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydroCUBE6Tests/HydroCUBE6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HydroCUBE6Tests.swift
+//  HydroCUBE6Tests
+//
+//  Created by Elliott Fogiel on 4/11/25.
+//
+
+import Testing
+@testable import HydroCUBE6
+
+struct HydroCUBE6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
